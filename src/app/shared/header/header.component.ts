6cbcbfb72a57aa1da294c661719f96f84f1d7e1f
Blake Shetter <BlakeShetter@RamSoftware.local>
2017-05-03 14:16:40 -0400

    finish cleanup

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../services/user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService:UserService) { }
 
   ngOnInit() {
   }
